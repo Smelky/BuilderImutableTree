5cbbfd84b21ba7d8185c1ceb843995e166df5110
Smelky <46086711+Smelky@users.noreply.github.com>
2019-01-30 15:57:44 +0200

    add

---
+++ src/com/study/Immutable/Main.java
@@ -0,0 +1,9 @@
+package com.study.Immutable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person("Vasia", "Pupkin");
+        System.out.println(person.getName());
+    }
+}

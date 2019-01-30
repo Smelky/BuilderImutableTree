851275ae53545ae3b4e62181861f20ed02dbe649
Smelky <46086711+Smelky@users.noreply.github.com>
2019-01-30 15:54:16 +0200

    add

---
+++ src/com/study/Immutable/Person.java
@@ -0,0 +1,20 @@
+package com.study.Immutable;
+
+public final class Person {
+
+    private final String name;
+    private final String surname;
+
+    public Person(final String name, final String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+}
